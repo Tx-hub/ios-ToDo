b4ec3a4d877e4b922ebb2ec0fb77d0b306e6f44c
2648216482@qq.com <Tu19980625>
2021-02-16 12:02:40 +0800

    Initial Commit

---
+++ toDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  toDoList
+//
+//  Created by tuxiao on 2021/2/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by tuxiao on 2021/2/16.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
